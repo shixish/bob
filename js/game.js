fcb8f349c69b14d1d163a1cde2723044b434b071
Andrew <shixish@gmail.com>
2013-08-16 22:20:54 -1000

    Replaced all files with the neverwell moor code

--- js/game.js
+++ js/game.js
@@ -1,42 +1,285 @@
+/*
+ * Neverwell Moor, a fantasy action RPG
+ * Copyright (C) 2012  Jason Oster
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* Game namespace */
 var game = {
+    // Whether a dialog box is waiting for input.
+    "modal" : false,
+
+    // `true` when an object's y-coordinate changes to put it at the proper Z-order.
+    "wantsResort" : false,
+
     // Run on page load.
-    "onload" : function () {
+    "onload" : function onload() {
         // Initialize the video.
-        if (!me.video.init("screen", 480, 320, true, 'auto')) {
+        if (!me.video.init("screen", c.WIDTH, c.HEIGHT)) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
-		
-		// add "#debug" to the URL to enable the debug Panel
-		if (document.location.hash === "#debug") {
-			window.onReady(function () {
-				me.plugin.register.defer(debugPanel, "debug");
-			});
-		}
 
         // Initialize the audio.
-        me.audio.init("mp3,ogg");
+        me.audio.init("ogg");
+
+        // Key bindings.
+        me.input.bindKey(me.input.KEY.UP,       "up");
+        me.input.bindKey(me.input.KEY.LEFT,     "left");
+        me.input.bindKey(me.input.KEY.DOWN,     "down");
+        me.input.bindKey(me.input.KEY.RIGHT,    "right");
+        me.input.bindKey(me.input.KEY.W,        "up");
+        me.input.bindKey(me.input.KEY.A,        "left");
+        me.input.bindKey(me.input.KEY.S,        "down");
+        me.input.bindKey(me.input.KEY.D,        "right");
+        me.input.bindKey(me.input.KEY.ENTER,    "action", true);
+        me.input.bindKey(me.input.KEY.SPACE,    "action", true);
+        me.input.bindKey(me.input.KEY.SHIFT,    "shift");
+        me.input.bindKey(me.input.KEY.Z,        "attack", true);
+        me.input.bindKey(c.KEY_APOS,            "attack", true);
+        me.input.bindKey(me.input.KEY.ESC,      "skip", true);
 
         // Set a callback to run when loading is complete.
         me.loader.onload = this.loaded.bind(this);
-     
-        // Load the resources.
-        me.loader.preload(game.resources);
+        this.loadResources();
 
         // Initialize melonJS and display a loading screen.
         me.state.change(me.state.LOADING);
     },
 
+    "loadResources" : function loadResources() {
+        // Set all resources to be loaded.
+        var resources = [];
 
+        // Graphics.
+        this.resources["img"].forEach(function forEach(value) {
+            resources.push({
+                "name"  : value,
+                "type"  : "image",
+                "src"   : "resources/img/" + value + ".png"
+            })
+        });
+
+        // Maps.
+        this.resources["map"].forEach(function forEach(value) {
+            resources.push({
+                "name"  : value,
+                "type"  : "tmx",
+                "src"   : "resources/map/" + value + ".tmx"
+            })
+        });
+
+        // Sound effects.
+        this.resources["sfx"].forEach(function forEach(value) {
+            resources.push({
+                "name"      : value,
+                "type"      : "audio",
+                "src"       : "resources/sfx/",
+                "channel"   : 1
+            })
+        });
+
+        // Music.
+        this.resources["bgm"].forEach(function forEach(value) {
+            resources.push({
+                "name"      : value,
+                "type"      : "audio",
+                "src"       : "resources/bgm/",
+                "channel"   : 2
+            })
+        });
+
+        // Load the resources.
+        me.loader.preload(resources);
+    },
 
     // Run on game resources loaded.
-    "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+    "loaded" : function loaded() {
+        // Create a notifier.
+        game.notify = new game.Notify();
 
-        // Start the game.
-        me.state.change(me.state.PLAY);
-    }
+        // Set the "Play" ScreenObject.
+        game.play = new game.PlayScreen(20);
+        me.state.set(me.state.PLAY, game.play);
+
+        // Set the TitleScreen ScreenObject.
+        me.state.set(me.state.MENU, new game.TitleScreen());
+
+        // Set the Story ScreenObject.
+        me.state.set(c.STATE_INTRO, new game.InfoScreen(
+            game.story.intro,
+            me.state.PLAY,
+            "black",
+            1000,
+            true
+        ));
+
+        // Set the GameOver ScreenObject.
+        me.state.set(me.state.GAMEOVER, new game.InfoScreen(
+            game.story.gameover,
+            me.state.PLAY,
+            "black",
+            1000
+        ));
+
+        // Set the Credits ScreenObject.
+        me.state.set(me.state.CREDITS, new game.CreditsScreen());
+
+        // Player entity.
+        me.entityPool.add("rachel", game.RachelEntity);
+
+        // NPCs.
+        me.entityPool.add("abi", game.NPCEntities.Abi);
+        me.entityPool.add("george", game.NPCEntities.George);
+        me.entityPool.add("jessica", game.NPCEntities.Jessica);
+        me.entityPool.add("whitey", game.NPCEntities.Whitey);
+
+        // Baddies.
+        me.entityPool.add("snake", game.BaddieEntities.Snake);
+
+        // Collectibles.
+        me.entityPool.add("coin_gold", game.CoinEntity);
+        me.entityPool.add("coin_silver", game.CoinEntity);
+
+        // Interactive objects.
+        me.entityPool.add("chest", game.ChestEntity);
+        me.entityPool.add("item", game.ItemEntity);
+
+        // Static objects.
+        me.entityPool.add("exit", game.Exit);
+        me.entityPool.add("static", game.Static);
+
+        if (c.DEBUG) {
+            me.state.change(me.state.PLAY);
+        }
+        // Display warning if audio is not available.
+        else if (!me.audio.isAudioEnable()) {
+            me.state.set(c.STATE_INFO, new game.InfoScreen(game.info.audio_error));
+            me.state.change(c.STATE_INFO);
+        }
+        else {
+            // Start the game.
+            me.state.change(me.state.MENU);
+        }
+    },
+
+    // Helper function to determine if a variable is an Object.
+    "isObject" : function isObject(object) {
+        try {
+            return (!Array.isArray(object) && Object.keys(object));
+        }
+        catch (e) {
+            return false;
+        }
+    },
+
+    // Helper function to sort objects by `z` property, then `y` property.
+    "sort" : function sort(a, b) {
+        var result = (b.z - a.z);
+        return (result ? result : ((b.pos && b.pos.y) - (a.pos && a.pos.y)) || 0);
+    },
+
+    // Helper function to get an image with error checking.
+    "getImage" : function getImage(name) {
+        var result = me.loader.getImage(name);
+        if (!result) {
+            throw "Error: No image named `" + name + "` (Did you forget to include the resource?)";
+        }
+        return result;
+    },
+
+    // Simple quests make the game interesting!
+    "quests" : (function quests() {
+        var all = [];
+        var subscribed = [];
+
+        /**
+         * Update quest progress.
+         *
+         * @param {String} event
+         *      Process all quests listening for `event`.
+         */
+        function progress(event, quantity) {
+            // Iterate over all active quests on the queue.
+            all.forEach(function forEach(quest, key) {
+                // When the quest is waiting for this event...
+                if (quest.list.hasOwnProperty(event)) {
+                    // Update the quantity...
+                    quest.list[event] -= +quantity;
+
+                    // If the quota has been fulfilled...
+                    if (quest.list[event] <= 0) {
+                        // Remove the event...
+                        delete quest.list[event];
+
+                        // Notify...
+                        if (typeof(quest.callback) === "function") {
+                            quest.callback();
+                        }
+
+                        // And if all events have fulfilled their quota...
+                        if (!Object.keys(quest.list).length) {
+                            // Remove the quest from this listener...
+                            all.splice(key, 1);
+                        }
+                    }
+                }
+            });
+        }
+
+        return {
+            /**
+             * Add a new quest.
+             *
+             * @param {Array} list
+             *      Array of quest events to subscribe to.
+             * @param {Function}
+             *      callback Called when all events have been received.
+             */
+            "add" : function add_quest(list, callback) {
+                // Add this quest to the queue.
+                all.push({
+                    "list"      : list,
+                    "callback"  : callback
+                });
+
+                // Check for new subscriptions.
+                Object.keys(list).forEach(function eachKey(item) {
+                    if (subscribed.indexOf(item) === -1) {
+                        subscribed.push(item);
+
+                        me.event.subscribe(item, (function subscriptionFactory(event) {
+                            return function onPublish() {
+                                progress.apply(progress, [ event ].concat(
+                                    Array.prototype.slice.call(arguments))
+                                );
+                            };
+                        })(item));
+                    }
+                });
+            },
+
+            /**
+             * Get all active quests
+             * @return {Array}
+             *      Complete list of active quests.
+             */
+            "getAll" : function get_quests() {
+                // Return a copy; don't let callers modify internal state.
+                return all.slice(0);
+            }
+        };
+    })()
 };
--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,594 @@
-game.resources = [
+/*
+ * Neverwell Moor, a fantasy action RPG
+ * Copyright (C) 2012  Jason Oster
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
-	
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
+game.resources = {
+    /* Graphics. */
+    "img" : [
+        // UI
+        "dialog",
+        "heart_empty",
+        "heart_half",
+        "heart_full",
+        "logo",
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
-	 */	
-	
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
-	 */
+        // Characters
+        "rachel",
+        "rachel_eyes",
+        "abi",
+        "abi_eyes",
+        "george",
+        "george_eyes",
+        "jessica",
+        "jessica_eyes",
+        "whitey",
+        "whitey_eyes",
+        "char_shadow",
+
+        // Baddies
+        "snake",
+
+        // Collectibles
+        "coin_gold",
+        "coin_silver",
+        "coin_shadow",
+        "hammer",
+
+        // Interactive objects
+        "chests",
+
+        // Tile maps
+        /* island */
+        "animwater",
+        "grass",
+        "sandwater",
+        "treetop",
+        "trunk",
+
+        /* rachels_room */
+        "accessories",
+        "cabinets",
+        "country",
+        "floor",
+        "house",
+        "inside",
+        "stairs",
+        "victoria",
+
+        /* rachels_house */
+        "kitchen",
+
+        /* earth */
+        "barrel",
+        "bridges",
+        "buckets",
+        "dirt",
+        "dirt2",
+        "doors",
+        "farming_fishing",
+        "fence",
+        "fence_alt",
+        "flowers_2",
+        "grassalt_flowers",
+        "housey",
+        "misc",
+        "mountains",
+        "shadow",
+        "signs",
+        "stonepattern",
+        "town_buildings",
+        "tree_stump",
+        "victorian_house",
+        "windmill"
+    ],
+
+    /* Maps from Tiled. */
+    "map" : [
+        "island",
+        "earth",
+        "general_store",
+        "rachels_room",
+        "rachels_house",
+        "shed"
+    ],
+
+    /* 1-channel audio. Usually sound effects. */
+    "sfx" : [
+        "chests",
+        "collect_coin",
+        "dying",
+        "fanfare",
+        "hurt",
+        "mallet_swing",
+        "mallet_whomp"
+    ],
+
+    /* 2-channel audio. Usually music. */
+    "bgm" : [
+        "bells",
+        "del_erad",
+        "pink_and_lively",
+        "random_and_cheap"
+    ]
+};
+
+game.info = {
+    "audio_error" : [
+        {
+            "messages" : [
+                "Your browser does not support Ogg-Vorbis audio.",
+                "Sounds have been disabled.",
+                "",
+                "Press [Enter] or [Space] to continue."
+            ]
+        }
+    ]
+};
+
+game.story = {
+    "intro" : [
+        {
+            "image" : null,
+            "messages" : [
+                "Papa used to tell me stories of an island he called Neverwell.",
+                "Some of his stories were frightening, but they always",
+                "had a happy ending."
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "He would tell me of the people that lived on Neverwell. How",
+                "different they were from us; Some could harness the power of",
+                "magic in their bare hands. And even the youngest weren't",
+                "afraid to go adventuring."
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "There were monsters, treasures buried in deep caves, and",
+                "thieves hiding in the mountains. And he had seen it all."
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "Papa would talk about so many things that it seemed impossible",
+                "to put it all on one island. I guess that's why he set some of",
+                "his adventures on smaller islands around the mainland."
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "I remember the moor on Neverwell held a special place in",
+                "Papa's heart. He said it was the most dangerous place he",
+                "ever saw, but that it held some kind of a secret...",
+                "Maybe it was the source of the inhabitants' magic?"
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "Papa died 10 years ago, so I'll never know the answer.",
+                "I'm 15 now."
+            ]
+        },
+        {
+            "image" : null,
+            "messages" : [
+                "These days I help my best friend Jessica with chores. That's",
+                "about as adventurous as I get. We live on an island we",
+                "call Earth. The only island there is..."
+            ]
+        }
+    ],
+
+    "gameover" : [
+        {
+            "image" : null,
+            "messages" : [
+                "Rachel ... Rachel? Is that you, Rachel?",
+                "It isn't your time yet ...",
+                "",
+                "You're going back, now, Rachel.",
+                "This will all seem like just a dream ...",
+                "Good luck, Rachel.",
+                "I love you ...",
+                "",
+                "Press [Enter] or [Space] to continue."
+            ]
+        }
+    ]
+};
+
+game.credits = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Concept :",
+    "                                   ... Jason Oster",
+    "    http://www.kodewerx.org/",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Programming :",
+    "",
+    "",
+    "  Neverwell Moor",
+    "                                   ... Jason Oster",
+    "    http://www.kodewerx.org/",
+    "    *The MIT License",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "  melonJS-0.9.4.js",
+    "  melonJS                          ... Olivier Biot, et al.",
+    "    http://www.melonjs.org/",
+    "    *The MIT License",
+    "",
+    "  cp.js",
+    "  Chipmunk-js                      ... Scott Lembcke, Joseph Gentle, et al.",
+    "    https://github.com/josephg/Chipmunk-js/",
+    "    *The MIT License",
+    "",
+    "  minpubsub.js",
+    "  MinPubSub                        ... Daniel Lamb",
+    "    https://github.com/daniellmb/MinPubSub/",
+    "    *The MIT License",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Original Art Work :",
+    "",
+    "",
+    "  flowers_2.png",
+    "  housey.png",
+    "  town_buildings.png",
+    "                                   ... Guido Bos",
+    "    http://opengameart.org/content/lpc-guido-bos-entries-cut-up",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  dialog.png",
+    "                                   ... Thane Brimhall (AKA Pennomi)",
+    "    http://opengameart.org/content/lpc-pennomis-ui-elements",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  doors.png",
+    "                                   ... Pol Camara (AKA PolCPP)",
+    "    http://opengameart.org/comment/reply/10851/11360",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  abi.png",
+    "  abi_eyes.png",
+    "  george.png",
+    "  george_eyes.png",
+    "  jessica.png",
+    "  jessica_eyes.png",
+    "  rachel.png",
+    "  rachel_eyes.png",
+    "  whitey.png",
+    "  whitey_eyes.png",
+    "                                   ... Radomir Dopieralski (AKA Sheep)",
+    "    http://opengameart.org/lpc-art-entries",
+    "    http://opengameart.org/content/one-more-lpc-alternate-character",
+    "    http://opengameart.org/content/alternate-lpc-character-sprites-george",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  sandwater.png",
+    "  farming_fishing.png",
+    "  fence.png",
+    "  fence_alt.png",
+    "                                   ... Daniel Eddeland",
+    "    http://opengameart.org/content/lpc-farming-tilesets-magic-animations-",
+    "    and-ui-elements",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  grassalt_flowers.png",
+    "  stonepattern.png",
+    "  victorian_house.png",
+    "  windmill.png",
+    "                                   ... Casper Nilsson",
+    "    http://opengameart.org/lpc-art-entries",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  snake.png",
+    "                                   ... Charles Sanchez (AKA CharlesGabriel)",
+    "    http://lpc.opengameart.org/static/lpc-style-guide/authors.html",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  barrel.png",
+    "  buckets.png",
+    "  bridges.png",
+    "  cabinets.png",
+    "  chests.png",
+    "  country.png",
+    "  dirt2.png",
+    "  dirt.png",
+    "  grass.png",
+    "  house.png",
+    "  inside.png",
+    "  kitchen.png",
+    "  mountains.png",
+    "  shadow.png",
+    "  signs.png",
+    "  stairs.png",
+    "  treetop.png",
+    "  trunk.png",
+    "  victoria.png",
+    "  watergrass.png",
+    "                                   ... Lanea Zimmerman (AKA Sharm)",
+    "    http://lpc.opengameart.org/static/lpc-style-guide/authors.html",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0 or later",
+    "",
+    "",
+    "  accessories.png",
+    "                                   ... Thane Brimhall (AKA Pennomi)",
+    "    http://opengameart.org/content/lpc-tabletop-rpg-pizza",
+    "                                   ... Jetrel",
+    "    http://opengameart.org/content/rpg-item-set",
+    "    *CC0",
+    "",
+    "",
+    "  heart_empty.png",
+    "  heart_full.png",
+    "  heart_half.png",
+    "                                   ... Casper Nilsson",
+    "    http://opengameart.org/content/simple-small-pixel-hearts",
+    "    *CC-BY-SA 3.0",
+    "",
+    "",
+    "  floor.png",
+    "                                   ... Stephen Challener (AKA Redshrike)",
+    "    http://opengameart.org/content/16x16-indoor-rpg-tileset-the-baseline",
+    "    *CC-BY-SA 3.0",
+    "",
+    "",
+    "  misc.png",
+    "                                   ... Matthew Nash",
+    "    http://opengameart.org/content/misc-32x32-tiles",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0",
+    "    *GNU GPL 2.0",
+    "",
+    "",
+    "  coin_gold.png",
+    "  coin_silver.png",
+    "  coin_shadow.png",
+    "                                   ... Chris Bellanger",
+    "    http://opengameart.org/content/animated-coins",
+    "    *CC-BY 3.0",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0",
+    "    *GNU GPL 2.0",
+    "",
+    "",
+    "  char_shadow.png",
+    "  logo.png",
+    "  hammer.png",
+    "                                   ... Jason Oster",
+    "    *CC0",
+    "",
+    "",
+    "  Small modifications",
+    "                                   ... Jason Oster",
+    "    *CC0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Music :",
+    "",
+    "",
+    "  bells.ogg",
+    "                                   ... Nelson James Gatlin",
+    "    http://opengameart.org/lpc-art-entries",
+    "    *CC-BY-SA 3.0",
+    "    *GNU GPL 3.0",
+    "",
+    "",
+    "  del_erad.ogg",
+    "                                   ... Janne Hanhisuanto for Radakan",
+    "    http://opengameart.org/content/radakan-del-erad",
+    "    *CC-BY-SA 3.0",
+    "",
+    "",
+    "  pink_and_lively.ogg",
+    "  random_and_cheap.ogg",
+    "                                   ... atrk-bu (Auto-generated)",
+    "    https://github.com/iamgreaser/it2everything/blob/master/atrk-bu.py",
+    "    *CC-0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Sound Effects :",
+    "",
+    "",
+    "  fanfare.ogg",
+    "                                   ... jivatma07",
+    "    http://www.freesound.org/people/jivatma07/sounds/122255/",
+    "    *CC-BY-SA 3.0",
+    "",
+    "",
+    "  mallet_swing.ogg",
+    "  mallet_whomp.ogg",
+    "                                   ... Etherian",
+    "    http://opengameart.org/comment/reply/11373/12927",
+    "",
+    "",
+    "  chests.ogg",
+    "  collect_coin.ogg",
+    "  dying.ogg",
+    "  hurt.ogg",
+    "                                   ... cfxr (Auto-generated)",
+    "    http://thirdcog.eu/apps/cfxr/",
+    "    *CC-0",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Level Design :",
+    "                                   ... Jason Oster",
+    "                                   ... Etherian",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "A Liberated Pixel Cup Production",
+    "2012 Jason Oster",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "                             For my Favorite",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
 ];
