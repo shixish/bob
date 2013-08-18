0973e854eb6b0e29d9792f877cca17ef6bbd371d
Forest Bremer <forest.bremer@gmail.com>
2013-08-17 19:49:54 -1000

    working on path finding pre recs
    need to get destination tile info

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,6 +13,38 @@ game.Marine = me.ObjectEntity.extend({
 	/*
 	 * constructor
 	 */
+	 init: function(x, y, settings) {
+	 
+	 
+	 }
+	 
+	/*
+	 * select destination
+	 */
+	 var clickX; // click position
+	 var clickY; // click position
+	 var destX; // tile index
+	 var destY; // tile index
+	 
+	me.input.registerMouseEvent('mousedown', me.game.viewport, this.clicked.bind(this)); // this should be a function thingy
+	
+		// to get the collision layer
+		var layer = me.game.currentLevel.getLayerByName("collision");
+		 
+		// to get the Tile
+		var myTile = layer.getTile(clickX, clickY);
+		destX = myTile.x; // tile index
+		destY = myTile.y; // tile index
+		
+		// access tile properties
+		var myTile = layer.getTileProperties(myTile.tileId);
+		
+		// check if tile is acceptable destination (is passable)
+		if (myTile.type != 'solid') {
+			// TODO: path finding
+		}
+	
+	
 	 
 	/* 
 	 * update position
